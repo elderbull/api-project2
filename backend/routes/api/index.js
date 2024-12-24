e71d2a1630d96bf5014cf7f985e396e83598eb42
Leon <lnathan.bull@gmail.com>
2024-12-23 19:23:05 -0500

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,17 +1,24 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js')
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
   router.use(restoreUser);
-  
-//Test route
-/* router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  }); */
 
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+//Test route
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
 
 
 module.exports = router;
