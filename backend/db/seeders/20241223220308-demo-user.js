18ccf21db7bec372941c6501e6d16ffe8909fc13
Leon <lnathan.bull@gmail.com>
2024-12-24 01:13:59 -0500

    Added firstName and lastName attributes to user seeder file

--- backend/db/seeders/20241223220308-demo-user.js
+++ backend/db/seeders/20241223220308-demo-user.js
@@ -13,16 +13,22 @@ module.exports = {
     await User.bulkCreate([
       {
         email: 'demo@user.io',
+        firstName: 'Demo',
+        lastName: 'User',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
+        firstName: 'User',
+        lastName: '1',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
+        firstName: 'User',
+        lastName: '2',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
       }
