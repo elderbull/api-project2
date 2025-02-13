ba2c9af71a57db6d379c6e18d7c524a08f134d74
Leon <lnathan.bull@gmail.com>
2025-02-12 22:35:23 -0500

    Wrapping up create a spot form. Need to finish styling

--- frontend/src/components/CreateSpotFormPage/CreateSpotFormPage.jsx
+++ frontend/src/components/CreateSpotFormPage/CreateSpotFormPage.jsx
@@ -71,7 +71,7 @@ function CreateSpotFormPage() {
         .catch(async (res) => {
           const data = await res.json();
           if (data?.errors) {
-            setErrors(data.errors);
+            // setErrors(data.errors);
           }
         });
 
@@ -81,8 +81,145 @@ function CreateSpotFormPage() {
     return (
         <div className='create-spot-box'>
             <h1>Create a New Spot</h1>
+            <form onSubmit={handleSubmit} className='spot-form'>
+                <div className='new-spot-section'>
+                    <h2 className='spotform-heading'>Where's your place located?</h2>
+                    <p className='spotform-caption'>Guests will only get your exact address once they book a reservation</p>
+                    <input
+                        placeholder='Country'
+                        className='form-input'
+                        type="text"
+                        value={country}
+                        onChange={(e) => setCountry(e.target.value)}
+                    />
+                    {errors.country && <p className='input-error'>{errors.country}</p>}
+                    <input
+                        placeholder='Street Address'
+                        className='form-input'
+                        type="text"
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                    />
+                    {errors.address && <p className='input-error'>{errors.address}</p>}
+                    <input
+                        placeholder='City'
+                        className='form-input'
+                        type="text"
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                    {errors.city && <p className='input-error'>{errors.city}</p>},
+                    <input
+                        placeholder='State'
+                        className='form-input'
+                        type="text"
+                        value={state}
+                        onChange={(e) => setState(e.target.value)}
+                    />
+                    {errors.state && <p className='input-error'>{errors.state}</p>}
+                    <input
+                        placeholder='Latitude'
+                        className='form-input'
+                        type="text"
+                        value={lat}
+                        onChange={(e) => setLat(e.target.value)}
+                    />
+                    {errors.lat && <p className='input-error'>{errors.lat}</p>}
+                    <input
+                        placeholder='Longitude'
+                        className='form-input'
+                        type="text"
+                        value={lng}
+                        onChange={(e) => setLng(e.target.value)}
+                    />
+                    {errors.lng && <p className='input-error'>{errors.lng}</p>}
+                </div>
+                <div className='new-spot-section'>
+                    <h2 className='spotform-heading'>Describe your place to guests</h2>
+                    <p className='spotform-caption'>Mention the best features of your space, any special amenties like fast ifi or parking, and what you about the neighborhood.</p>
+                    <textarea
+                        placeholder='Please write at least 30 characters'
+                        className='form-input'
+                        type="text"
+                        rows="7"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                    {errors.description && <p className='input-error'>{errors.description}</p>}
+                </div>
+                <div className='new-spot-section'>
+                    <h2 className='spotform-heading'>Create a title for your spot</h2>
+                    <p className='spotform-caption'>Catch guests' attention with a spot title that highlights what makes your place special.</p>
+                    <input
+                        placeholder='Name of your spot'
+                        className='form-input'
+                        type='text'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    {errors.name && <p className='input-error'>{errors.name}</p>}
+                </div>
+                <div className='new-spot-section'>
+                <h2 className='spotform-heading'>Set a base price for your spot</h2>
+                <p className='spotform-caption'>Competitive pricing can help your listing stand out and rank higher in search results.</p>
+                $ <input
+                    placeholder='Price per night (USD)'
+                    className='form-input price-input'
+                    type='number'
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                 />
+                </div>
+                <div className='new-spot-section'>
+                <h2 className='spotform-heading'>Liven up your spot with photos</h2>
+                <p className='spotform-caption'>Submit a link to a least one photo to publish</p>
+                <input
+                    placeholder='Preview Image URL'
+                    className='form-input'
+                    type='url'
+                    value={previewImgUrl}
+                    onChange={(e) => setPreviewImgUrl(e.target.value)}
+                 />
+                <input
+                    placeholder='Image URL'
+                    className='form-input'
+                    type='url'
+                    value={img1Url}
+                    onChange={(e) => setImg1Url(e.target.value)}
+                 />
+                <input
+                    placeholder='Image URL'
+                    className='form-input'
+                    type='url'
+                    value={img2Url}
+                    onChange={(e) => setImg2Url(e.target.value)}
+                 />
+                <input
+                    placeholder='Image URL'
+                    className='form-input'
+                    type='url'
+                    value={img3Url}
+                    onChange={(e) => setImg3Url(e.target.value)}
+                 />
+                <input
+                    placeholder='Image URL'
+                    className='form-input'
+                    type='url'
+                    value={img4Url}
+                    onChange={(e) => setImg4Url(e.target.value)}
+                 />
+                </div>
+
+                <div className='create-spot-bttn-bx'>
+                    <button type="submit" id="newspot-submit-bttn">
+                        Create Spot
+                    </button>
+                </div>
 
 
+
+
+            </form>
         </div>
     )
 }
